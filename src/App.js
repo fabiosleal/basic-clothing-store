3207b83ca8b52dd3e61db934612e238f73aaa99e
Fabio Leal <fabio.lael@gmail.com>
2020-05-19 04:20:39 -0300

    broken aour homepage into smaller components, finished styling aour menu-item component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import HomePage from "./components/homepage/homepage.component";
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
-    return (
-        <div>
-            <HomePage />
-        </div>
-    );
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
